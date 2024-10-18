96e4ae0b8ce0fc9ccd74b6e007486daff7c6b0ec
Alok Skj <alokskj14@gmail.com>
2024-10-18 10:46:55 +0530

    Update Header.tsx

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         <header className="px-4 h-16 lg:px-6  flex items-center justify-between  z-10 text-black wrapper">
             <div className="flex items-center gap-6 ">
                 <Logo />
-                <Navigation />
+{/*                 <Navigation /> */}
             </div>
             {!isLoading &&
                 (user ? (
